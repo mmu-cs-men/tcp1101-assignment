5a4a465584a5140d1bbbc689befd13a03c5642d0
Laxman Pillai <laxman.pillai1104@gmail.com>
2024-01-20 02:15:48 +0800

    Added starting block for Lexer

---
+++ Lexer.cpp
@@ -0,0 +1,55 @@
+#include "Lexer.h"
+#include "Token.h"
+#include <iostream>
+
+/**
+ * Class Lexer Definition
+ * @param program String containing ASM code to be tokenized
+ * @note - inputProgram --> set to program string (which contains ASM code)
+ * @note - currentPosstion --> initialized to 0, indicating the start of tokenization process
+ */
+Lexer::Lexer(std::string program) : inputProgram(program), currentPosition(0)
+{
+}
+
+std::string Lexer::readWord()
+{   
+    std::string word;
+    
+    // Skips spases and commas in the start of the program  
+    while (currentPosition < inputProgram.length() && (inputProgram[currentPosition] == ' ') ||
+           (inputProgram[currentPosition] == ','))
+    {
+        currentPosition++;
+    }
+
+    // Reads word until space or comma or newline
+    while (currentPosition < inputProgram.length() &&
+           (inputProgram[currentPosition] != ' ' && inputProgram[currentPosition] != ',' &&
+            inputProgram[currentPosition] != '\n'))
+    {
+        word += inputProgram[currentPosition];
+        currentPosition++;
+
+    }
+
+    return word;
+}
+
+/**
+ * @note - Method from member of Lexer Class
+ * @note - Analyzes input program and breaks it down into tokens
+ * @note - Returns a vector containing object type Token
+ */
+std::vector<Token> Lexer::getTokens()
+{
+    // - vector that stores 'Token' as program processes input program(ASM Code)
+    std::vector<Token> tokens;
+
+    while (currentPosition < inputProgram.length())
+    {
+        std::string word = readWord();
+    }
+
+    return tokens;
+}
