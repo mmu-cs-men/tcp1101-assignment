0bb7a57a2dd42f7276aa3893d88f1bbe75da049b
Abdullah Borkan Hawash <abdullahhawash93@gmail.com>
2024-01-25 00:10:31 +0800

    change cerr to cout

--- Runner.cpp
+++ Runner.cpp
@@ -1,12 +1,16 @@
 #include "Runner.h"
 #include <iostream>
 
-Runner::Runner(MachineState &machineState) : machineState(machineState) {}
+Runner::Runner(MachineState &machineState) : machineState(machineState)
+{
+}
 
-void Runner::in(int registerNum) {
+void Runner::in(int registerNum)
+{
     // Check if the register number is within the valid range (0 to 6)
-    if (registerNum < 0 || registerNum > 6) {
-        std::cerr << "Error: Invalid register number" << std::endl;
+    if (registerNum < 0 || registerNum > 6)
+    {
+        std::cout << "Error: Invalid register number" << std::endl;
         return;
     }
 
@@ -15,8 +19,9 @@ void Runner::in(int registerNum) {
     std::cin >> value;
 
     // Ensure the value is within the 0-255 range
-    if (value < 0 || value > 255) {
-        std::cerr << "Error: Input value out of range" << std::endl;
+    if (value < 0 || value > 255)
+    {
+        std::cout << "Error: Input value out of range" << std::endl;
         return;
     }
 
