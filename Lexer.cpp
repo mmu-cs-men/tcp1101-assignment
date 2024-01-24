365d55ac6403627cedcb80dea632620c1fef83c6
Laxman Pillai <136797692+Laxman1104@users.noreply.github.com>
2024-01-24 19:01:27 +0800

    Add Lexer.cpp (#1)
    * added Lexer.cpp file
    
    * added Lexer.h file.
    
    * Finish Lexer.h
    
    * Fixed comments
    
    * Fixed misspelled variable (inputProgram)
    
    * Added readWord() and fixed comments.
    
    * Added more comments, YAYY
    
    * MORE COMMENTS YAYYY
    
    * Added starting block for Lexer
    
    * YEESS again comments
    
    * Handle UnaryOpcode token
    
    * Add Register Token Handler
    
    * Dont mind me, YES comments
    
    * Changed readWord() logic for handling Whitespace, comma , newline
    
    * Add 'Whitespace' 'Comma' 'Newline' handlers.
    
    * Add MovOpcode handler
    
    * Add ArithmeticOpcode handler
    
    * Add BitwiseOpce handler
    
    * Add SerialOpcode handler
    
    * Add OpenBracket handler
    
    * Add CloseBracket handler.
    
    * Add NumLiteral handler.
    
    * Add Eof handler.
    
    * Fixed some formating
    
    * Formating
    
    * Better handling of Eof
    
    * Small change to comments
    
    * Completed Lexer
    
    * Clean up
    
    * Test code for lexer.cpp

---
+++ Lexer.cpp
@@ -0,0 +1,152 @@
+#include "Lexer.h"
+#include "Token.h"
+#include "TokenType.h"
+#include <iostream>
+
+/**
+ * Class Lexer Definition
+ * @param program String containing ASM code to be tokenized
+ * @note - inputProgram --> set to program string (which contains ASM code)
+ * @note - currentPosstion --> initialized to 0, indicating the start of tokenization process
+ */
+Lexer::Lexer(std::string program) : inputProgram(program), currentPosition(0)
+{
+}
+
+std::string Lexer::readWord()
+{
+    if (inputProgram[currentPosition] == ' ')
+    {
+        // Handle whitespaces and only returns 1 whitespace eventhough program has multiple
+        while (currentPosition < inputProgram.length() && (inputProgram[currentPosition] == ' '))
+        {
+            currentPosition++;
+        }
+        return " ";
+    }
+
+    if (inputProgram[currentPosition] == ',' || inputProgram[currentPosition] == '\n')
+    {
+        /**
+         * new string containing object with lenght of 1
+         * inputProgram[currentPosition++] --> char to be placed in the new string
+         * Char of current posistion of inputProgram
+         * currentPosition++ --> increment to move to next char in inputProgram
+         */
+        return std::string(1, inputProgram[currentPosition++]);
+    }
+
+    // Handle open and close brackets as separate tokens
+    if (inputProgram[currentPosition] == '[' || inputProgram[currentPosition] == ']') {
+        return std::string(1, inputProgram[currentPosition++]);
+    }
+
+    // Reads word until hitting space , comma , newline
+    std::string word;
+    while (currentPosition < inputProgram.length() && inputProgram[currentPosition] != ' ' &&
+           inputProgram[currentPosition] != ',' && inputProgram[currentPosition] != '\n' &&
+           inputProgram[currentPosition] != '[' && inputProgram[currentPosition] != ']')
+    {
+        word += inputProgram[currentPosition++];
+    }
+    return word;
+}
+
+/**
+ * @note - Method from member of Lexer Class
+ * @note - Analyzes input program and breaks it down into tokens
+ * @note - Returns a vector containing object type Token
+ */
+std::vector<Token> Lexer::getTokens()
+{
+    // - vector that stores 'Token' as program processes input program(ASM Code)
+    std::vector<Token> tokens;
+
+    while (currentPosition < inputProgram.length())
+    {
+
+        // Read word from current position
+        std::string word = readWord();
+
+        // Whitespace handler
+        if (word == " ")
+        {
+            tokens.push_back(Token(TokenType::Whitespace, word));
+        }
+
+        // Comma handler
+        else if (word == ",")
+        {
+            tokens.push_back(Token(TokenType::Comma, word));
+        }
+
+        // Newline handler
+        else if (word == "\n")
+        {
+            tokens.push_back(Token(TokenType::Newline, word));
+        }
+
+        // UnaryOpcode handler
+        else if (word == "IN" || word == "OUT" || word == "INC" || word == "DEC")
+        {
+            tokens.push_back(Token(TokenType::UnaryOpcode, word));
+        }
+
+        // Register handler
+        else if (word == "R0" || word == "R1" || word == "R2" || word == "R3" || word == "R4" || word == "R5" ||
+                 word == "R6")
+        {
+            tokens.push_back(Token(TokenType::Register, word));
+        }
+
+        // MovOpcode handler
+        else if (word == "MOV")
+        {
+            tokens.push_back(Token(TokenType::MovOpcode, word));
+        }
+
+        // ArithmeticOpcode handler
+        else if (word == "ADD" || word == "SUB" || word == "MUL" || word == "DIV")
+        {
+            tokens.push_back(Token(TokenType::ArithmeticOpcode, word));
+        }
+
+        // BitwiseOpcode handler
+        else if (word == "ROL" || word == "ROR" || word == "SHL" || word == "SHR")
+        {
+            tokens.push_back(Token(TokenType::BitwiseOpcode, word));
+        }
+
+        // SerialOpcode handler
+        else if (word == "LOAD" || word == "STORE")
+        {
+            tokens.push_back(Token(TokenType::SerialOpcode, word));
+        }
+
+        // OpenBracket handler
+        else if (word == "[")
+        {
+            tokens.push_back(Token(TokenType::OpenBracket, word));
+        }
+
+        // CloseBracket handler
+        else if (word == "]")
+        {
+            tokens.push_back(Token(TokenType::ClosedBracket, word));
+        }
+
+        // NumLiteral handler
+        else if (!word.empty() && std::isdigit(word[0]))
+        {
+            tokens.push_back(Token(TokenType::NumLiteral, word));
+        }
+    }
+
+    // Handling Eof
+    if (currentPosition >= inputProgram.length())
+    {
+        tokens.push_back(Token(TokenType::Eof, ""));
+    }
+
+    return tokens;
+}
---
+++ Lexer.h
@@ -0,0 +1,43 @@
+#pragma once
+#include <string>
+#include <vector>
+#include "Token.h"
+
+class Lexer 
+{
+    public:
+
+        /**
+         * Constructs a Lexer object.
+         * @param _program ASM code as a string to be tokenized.
+         */
+        Lexer(std::string _program);
+
+        /**
+         * @param  -None
+         * @brief  - Declaration of getTokens() method
+         * @note   - Process ASM code and identify tokens.
+         * @note   - Example: "IN  R0" will return a vector containing tokens UnaryOpcode, Whitespace, and Register.
+         * @return - vector <Token>.
+         */
+        std::vector<Token>getTokens();
+        
+    private:
+        // - Holds ASM program as string 
+        // - Input that lexer will process
+        std::string inputProgram;
+
+        // - Tracks current possition in the input program during tokenization. 
+        // - Determins where in the ASM code lexer is during tokenization process.
+        // - Initialized to 0
+        int currentPosition;
+
+        /**
+         * @brief   Declaration of readWord mehtod. 
+         * @note   - Operates on 'inputProgram' & 'currentPosition'
+         * @param  - None
+         * @return - Type string.
+        */
+        std::string readWord();
+
+};
