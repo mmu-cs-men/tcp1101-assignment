7f0edb95b442255a6db3e8bc20fa48a1d83a6848
Harris Majeed <harrismajeed2004@gmail.com>
2024-01-25 17:44:57 +0800

    Initial implementation: main program

---
+++ main.cpp
@@ -0,0 +1,37 @@
+#include "Lexer.h"
+#include "MachineState.h"
+#include "Parser.h"
+#include "Runner.h"
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+
+int main()
+{
+    std::string filePath;
+    std::cout << "Please enter path to an .asm file. You may enter an absolute "
+                 "or relative path."
+              << std::endl;
+    std::cout << "Input file path: ";
+    std::cin >> filePath;
+
+    std::ifstream file(filePath);
+    if (!file)
+    {
+        std::cout << "Unable to open file " << filePath << std::endl;
+        return 1;
+    }
+
+    std::string fileContent;
+    std::getline(file, fileContent, '\0');
+    file.close();
+
+    Lexer lexer(fileContent);
+    MachineState machineState;
+    Runner runner(machineState);
+    std::vector<Token> tokenList = lexer.getTokens();
+    Parser parser(tokenList, runner);
+    parser.parse();
+    machineState.dumpState();
+}
