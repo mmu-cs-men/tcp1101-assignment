64feae7ee33110c414fe2b820557afc0064a6741
Harris Majeed <harrismajeed2004@gmail.com>
2024-01-25 19:16:24 +0800

    fix formatting in dumpState()

--- MachineState.cpp
+++ MachineState.cpp
@@ -17,18 +17,30 @@ MachineState::MachineState()
 
 void MachineState::dumpState()
 {
-    std::cout << "Registers: ";
+    std::cout << std::setfill(' ') << std::left << std::setw(10) << "Registers"
+              << ": ";
     for (int i = 0; i < 7; i++)
     {
-        std::cout << std::setfill('0') << std::setw(2) << (int)registers[i]
-                  << " ";
+        if (i == 6)
+        {
+            std::cout << std::setfill('0') << std::setw(2) << (int)registers[i];
+        }
+        else
+        {
+            std::cout << std::setfill('0') << std::setw(2) << (int)registers[i]
+                      << " ";
+        }
     }
     std::cout << "#" << std::endl;
 
-    std::cout << "Flags: " << overflowFlag << " " << carryFlag << " "
-              << underflowFlag << " " << zeroFlag << " #" << std::endl;
-    std::cout << "PC: " << std::dec << (int)programCounter << std::endl;
-    std::cout << "Memory :" << std::endl;
+    std::cout << std::setfill(' ') << std::left << std::setw(10) << "Flags"
+              << ": " << overflowFlag << " " << carryFlag << " "
+              << underflowFlag << " " << zeroFlag << "#" << std::endl;
+    std::cout << std::setfill(' ') << std::left << std::setw(10) << "PC"
+              << ": " << std::dec << (int)programCounter << std::endl
+              << std::endl;
+    std::cout << std::setfill(' ') << std::left << std::setw(10) << "Memory"
+              << ":" << std::endl;
 
     for (int i = 0; i < 64; i++)
     {
@@ -36,5 +48,6 @@ void MachineState::dumpState()
             std::cout << std::endl;
         std::cout << std::setfill('0') << std::setw(2) << (int)memory[i] << " ";
     }
+    std::cout << std::endl;
     std::cout << "#" << std::endl;
 }
