851e197d865de91c68adea0b32f0d658b09fa72f
Harris Majeed <harrismajeed2004@gmail.com>
2024-01-27 22:47:00 +0800

    fix trailing zeros bug in dumpState()

--- MachineState.cpp
+++ MachineState.cpp
@@ -20,40 +20,35 @@ std::string MachineState::dumpState()
 {
     std::stringstream ss;
 
-    ss << std::setfill(' ') << std::left << std::setw(10) << "Registers"
-       << ": ";
-
+    // Dump registers
+    ss << "Registers : ";
     for (int i = 0; i < 7; i++)
     {
-        if (i == 6)
+        ss << std::setfill('0') << std::setw(2) << (int)registers[i];
+        if (i < 6)
         {
-            ss << std::setfill('0') << std::setw(2) << (int)registers[i];
-        }
-        else
-        {
-            ss << std::setfill('0') << std::setw(2) << (int)registers[i] << " ";
+            ss << " ";
         }
     }
-
     ss << "#" << std::endl;
-    ss << std::setfill(' ') << std::left << std::setw(10) << "Flags"
-       << ": " << overflowFlag << " " << carryFlag << " " << underflowFlag
-       << " " << zeroFlag << "#" << std::endl;
-    ss << std::setfill(' ') << std::left << std::setw(10) << "PC"
-       << ": " << std::dec << (int)programCounter << std::endl
-       << std::endl;
-    ss << std::setfill(' ') << std::left << std::setw(10) << "Memory"
-       << ":" << std::endl;
 
+    // Dump flags
+    ss << "Flags     : " << overflowFlag << " " << carryFlag << " "
+       << underflowFlag << " " << zeroFlag << "#" << std::endl;
+
+    // Dump program counter
+    ss << "PC        : " << std::dec << programCounter << std::endl
+       << std::endl;
+
+    // Dump memory
+    ss << "Memory    :" << std::endl;
     for (int i = 0; i < 64; i++)
     {
         if (i % 8 == 0 && i != 0)
             ss << std::endl;
         ss << std::setfill('0') << std::setw(2) << (int)memory[i] << " ";
     }
-
-    ss << std::endl;
-    ss << "#" << std::endl;
+    ss << "\n#" << std::endl;
 
     return ss.str();
 }
